06b9b15ce636c5ea18485d0a86dc1204f7a1622d
Takuya-Fukumura <blackchoco1114@gmail.com>
2023-08-28 22:58:18 +0900

    ログ出力の仕組みを導入

--- src/main/java/com/example/divichart/controller/Index.java
+++ src/main/java/com/example/divichart/controller/Index.java
@@ -3,12 +3,18 @@ package com.example.divichart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 @RequestMapping("/")
 public class Index {
+
+    private static final Logger log = LoggerFactory.getLogger(Index.class);
+
     @GetMapping
     public String index() {
+        log.info("TOP画面表示");
         return "index";
     }
 }
