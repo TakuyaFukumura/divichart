103ec219edfd5b4e02c3ed98c87ae771fa51438d
Takuya-Fukumura <blackchoco1114@gmail.com>
2023-09-10 15:49:02 +0900

    配当テーブル追加

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -4,3 +4,10 @@ WHERE `id` = 1;
 INSERT INTO `account`
 (`id`,   `name`,  `password`) VALUES
 (1, 'admin', 'pass');
+
+DELETE FROM `dividend_history`
+WHERE `id` = 1;
+
+INSERT INTO `dividend_history`
+(`id`, `amount_received`, `receipt_date`) VALUES
+(   1,               120,   '2022-11-14');
