d178bef994c7d07fe3a9240447fe617c9bf0c1ce
Takuya-Fukumura <blackchoco1114@gmail.com>
2023-09-16 13:37:19 +0900

    グラフ描画用サービスクラスを作成

--- src/main/java/com/example/divichart/controller/LineChartController.java
+++ src/main/java/com/example/divichart/controller/LineChartController.java
@@ -1,8 +1,11 @@
 package com.example.divichart.controller;
 
+import com.example.divichart.service.LineChartService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,10 +16,14 @@ public class LineChartController {
 
     private static final Logger log = LoggerFactory.getLogger(LineChartController.class);
 
-    @GetMapping
-    public String index() {
-        log.debug("累計配当グラフ表示");
+    @Autowired
+    LineChartService service;
 
+    @GetMapping
+    public String index(Model model) {
+        log.debug("累計配当グラフ表示");
+        String chartData = service.getChartData("2020"); // TODO:年は変数にする
+        model.addAttribute("chartData", chartData);
         return "lineChart";
     }
 
---
+++ src/main/java/com/example/divichart/service/LineChartService.java
@@ -0,0 +1,20 @@
+package com.example.divichart.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LineChartService {
+
+    /*
+     * @return result グラフ描画用データ
+     */
+    public String getChartData(String year) {
+
+        String chartData = "";
+        // 該当する年のデータを取得。月別の合計(sum)で取りたい
+        // 累計の配列に作り直す
+        // コンマで連結する
+        return chartData;
+    }
+
+}
