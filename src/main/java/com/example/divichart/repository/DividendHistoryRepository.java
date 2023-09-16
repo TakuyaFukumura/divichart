08a276cb8049ec6fa32a89e4fd89dc70e672be90
Takuya-Fukumura <blackchoco1114@gmail.com>
2023-09-16 13:44:36 +0900

    月別配当金額を取得できるようにする

--- src/main/java/com/example/divichart/repository/DividendHistoryRepository.java
+++ src/main/java/com/example/divichart/repository/DividendHistoryRepository.java
@@ -2,7 +2,20 @@ package com.example.divichart.repository;
 
 import com.example.divichart.entity.DividendHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.sql.Date;
 
 public interface DividendHistoryRepository extends JpaRepository<DividendHistory, Long> {
-    // 後で書く
+    @Query(value = "SELECT SUM(amount_received) FROM dividend_history", nativeQuery = true)
+    BigDecimal getDividendSum();
+
+    @Query(value = "SELECT SUM(amount_received) FROM dividend_history " +
+            "WHERE receipt_date BETWEEN :startDate AND :endDate", nativeQuery = true)
+    BigDecimal getDividendSum(
+            @Param("startDate") Date startDate
+            ,@Param("endDate") Date endDate
+    );
 }
--- src/main/java/com/example/divichart/service/LineChartService.java
+++ src/main/java/com/example/divichart/service/LineChartService.java
@@ -1,9 +1,18 @@
 package com.example.divichart.service;
 
+import com.example.divichart.repository.DividendHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.Calendar;
 
 @Service
 public class LineChartService {
+
+    @Autowired
+    DividendHistoryRepository repository;
 
     /*
      * @return result グラフ描画用データ
@@ -12,9 +21,37 @@ public class LineChartService {
 
         String chartData = "";
         // 該当する年のデータを取得。月別の合計(sum)で取りたい
+        BigDecimal[] monthlyDividendSum = getMonthlyDividendSum(year);
         // 累計の配列に作り直す
         // コンマで連結する
         return chartData;
+    }
+
+    /*
+     * 月別配当金額合計を取得する
+     */
+    public BigDecimal[] getMonthlyDividendSum(String year) {
+        BigDecimal[] monthlyDividendSum = new BigDecimal[12];
+
+        for (int i = 0; i < 12; i++) {
+
+            String startMonth = String.format("%02d", i+1);
+
+            Date startDate = Date.valueOf(year + "-" + startMonth + "-01");
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(startDate);
+
+            calendar.add(Calendar.MONTH, 1); // 次月にする
+            calendar.add(Calendar.DATE, -1); // 1引いて月末にする
+
+            long timeInMilliSeconds = calendar.getTime().getTime();
+            Date endDate = new Date(timeInMilliSeconds);
+
+            BigDecimal dividendSum = repository.getDividendSum(startDate, endDate);
+            monthlyDividendSum[i] = dividendSum;
+        }
+        return monthlyDividendSum;
     }
 
 }
