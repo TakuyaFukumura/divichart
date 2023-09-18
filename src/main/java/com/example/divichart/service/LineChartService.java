b85b03ecf8b0e72b544d55bfb759665ff5d998dd
Takuya-Fukumura <blackchoco1114@gmail.com>
2023-09-18 19:18:51 +0900

    ロジックを共通化

--- src/main/java/com/example/divichart/service/LineChartService.java
+++ src/main/java/com/example/divichart/service/LineChartService.java
@@ -1,18 +1,11 @@
 package com.example.divichart.service;
 
-import com.example.divichart.repository.DividendHistoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.StringJoiner;
 
 @Service
-public class LineChartService {
-
-    @Autowired
-    DividendHistoryRepository repository;
+public class LineChartService extends BasicChartService {
 
     /**
      * グラフ描画用に、指定年の1月～12月までの配当累計を計算する
@@ -23,27 +16,6 @@ public class LineChartService {
         BigDecimal[] monthlyDividend = getMonthlyDividend(year);
         BigDecimal[] cumulativeDividend = getCumulativeDividend(monthlyDividend);
         return createChartData(cumulativeDividend);
-    }
-
-    /**
-     * 月別配当金額を取得する
-     * @param year 対象年
-     * @return 月別配当配列
-     */
-    private BigDecimal[] getMonthlyDividend(String year) {
-        BigDecimal[] monthlyDividend = new BigDecimal[12];
-
-        for (int i = 0; i < 12; i++) {
-            int month = i + 1;
-            String formattedMonth = String.format("%02d", month);
-
-            LocalDate startDate = LocalDate.parse(year + "-" + formattedMonth + "-01");
-            LocalDate endDate = startDate.plusMonths(1).minusDays(1);
-
-            BigDecimal dividendSum = repository.getDividendSum(startDate, endDate);
-            monthlyDividend[i] = dividendSum;
-        }
-        return monthlyDividend;
     }
 
     /**
@@ -60,49 +32,6 @@ public class LineChartService {
             cumulativeDividend[i] = sum;
         }
         return cumulativeDividend;
-    }
-
-    /**
-     * 受け取ったデータをグラフ描画用に合成する
-     * @param cumulativeDividend 合成したいデータ配列
-     * @return 合成した文字列 例）"1,2,3,4,5"
-     */
-    private String createChartData(BigDecimal[] cumulativeDividend) {
-        StringJoiner chartData = new StringJoiner(",");
-        for (BigDecimal dividend : cumulativeDividend) {
-            chartData.add(dividend.toString());
-        }
-        return chartData.toString();
-    }
-
-    /**
-     * 今年も含めて過去5年分の年（西暦）を取得する
-     * @return 年を表す配列
-     */
-    public String[] getRecentYears() {
-        String[] recentYears = new String[5];
-        int currentYear = LocalDate.now().getYear();
-
-        for (int i = 0; i < 5; i++) {
-            recentYears[i] = String.valueOf(currentYear - i);
-        }
-        return recentYears;
-    }
-    
-    /**
-     * 指定された数の最近の年を取得する。
-     *
-     * @param numOfYears 取得する年数
-     * @return 最近の年の文字列配列
-     */
-    public String[] getRecentYears(int numOfYears) {
-        String[] recentYears = new String[numOfYears];
-        int currentYear = LocalDate.now().getYear();
-
-        for (int i = 0; i < numOfYears; i++) {
-            recentYears[i] = String.valueOf(currentYear - i);
-        }
-        return recentYears;
     }
 
 }
