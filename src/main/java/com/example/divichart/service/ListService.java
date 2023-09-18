5afdffe9e4b4d6f41dc988ceeeb77e3978c2bb81
Takuya-Fukumura <blackchoco1114@gmail.com>
2023-09-19 00:13:28 +0900

    日本の証券コードが含まれてしまうバグを修正

--- src/main/java/com/example/divichart/service/ListService.java
+++ src/main/java/com/example/divichart/service/ListService.java
@@ -51,8 +51,9 @@ public class ListService {
             List<CSVRecord> recordList = parse.getRecords();
 
             for (CSVRecord record : recordList) {
-                if (!record.get("銘柄コード").isEmpty()) {
-                    String tickerSymbol = record.get("銘柄コード");
+                String stockCode = record.get("銘柄コード");
+                if (!stockCode.isEmpty() && stockCode.matches("^[A-Z]*$")) {
+                    String tickerSymbol = stockCode;
                     BigDecimal amountReceived = new BigDecimal(record.get("受取金額[円/現地通貨]"));
                     String rowDate = record.get("入金日");
                     String sqlDate = rowDate.replace("/", "-");
