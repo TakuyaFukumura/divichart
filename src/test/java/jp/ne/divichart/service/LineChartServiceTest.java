928e4d6267e3b396417233ee0924c2c6e0654e19
Takuya-Fukumura <blackchoco1114@gmail.com>
2023-09-27 21:58:17 +0900

    LineChartServiceのテストを追加

---
+++ src/test/java/jp/ne/divichart/service/LineChartServiceTest.java
@@ -0,0 +1,42 @@
+package jp.ne.divichart.service;
+
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineChartServiceTest {
+    private LineChartService LineChartService;
+
+    @BeforeEach
+    public void setUp() {
+        LineChartService = new LineChartService();
+    }
+
+    @Test
+    public void testGetCumulativeDividend() {
+        BigDecimal[] monthlyDividend = {
+                new BigDecimal("100.00"),
+                new BigDecimal("50.00"),
+                new BigDecimal("75.00"),
+                new BigDecimal("30.00")
+        };
+
+        BigDecimal[] cumulativeDividend = LineChartService.getCumulativeDividend(monthlyDividend);
+
+        // メソッドが正しい結果を返すかテストします
+        assertNotNull(cumulativeDividend);
+        assertEquals(monthlyDividend.length, cumulativeDividend.length);
+
+        // 累積データが正しいことをテストします
+        BigDecimal cumulativeSum = BigDecimal.ZERO;
+        for (int i = 0; i < monthlyDividend.length; i++) {
+            cumulativeSum = cumulativeSum.add(monthlyDividend[i]);
+            assertEquals(cumulativeSum, cumulativeDividend[i]);
+        }
+    }
+}
