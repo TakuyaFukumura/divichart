17700e6ce886b00724b3c24c4ddd07cc9bd739d7
Takuya-Fukumura <blackchoco1114@gmail.com>
2023-10-17 00:58:04 +0900

    手動入力用にInsertFormを追加

---
+++ src/main/java/click/divichart/bean/form/list/InsertForm.java
@@ -0,0 +1,18 @@
+package click.divichart.bean.form.list;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class InsertForm implements Serializable {
+    private String tickerSymbol; // ティッカー
+    private BigDecimal amountReceived; // 配当受取金額
+    private Date receiptDate; // 配当受取日
+}
