2c2b360600107cc78409b32949fbad239174b140
Takuya-Fukumura <blackchoco1114@gmail.com>
2023-10-19 23:19:44 +0900

    CsvInsertFormを使うように変更

--- src/main/java/click/divichart/controller/ListController.java
+++ src/main/java/click/divichart/controller/ListController.java
@@ -1,6 +1,7 @@
 package click.divichart.controller;
 
 import click.divichart.bean.entity.DividendHistory;
+import click.divichart.bean.form.list.CsvInsertForm;
 import click.divichart.bean.form.list.InsertForm;
 import click.divichart.service.ListService;
 import lombok.extern.slf4j.Slf4j;
@@ -47,9 +48,11 @@ public class ListController {
     }
 
     @PostMapping("/csvInsert")
-    public String insert(@ModelAttribute("csvFile") MultipartFile csvFile) {
+    public String insert(CsvInsertForm csvInsertForm) {
         log.debug("配当履歴CSV一括登録");
-        service.csvInsert(csvFile);
+        service.csvInsert(
+                csvInsertForm.getCsvFile()
+        );
         return "redirect:/list";
     }
 }
