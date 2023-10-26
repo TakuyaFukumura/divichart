56f3a25af3ce4c84375ad672366e36df50741418
Takuya-Fukumura <blackchoco1114@gmail.com>
2023-10-26 22:22:05 +0900

    月別配当グラフ用DTO追加

---
+++ src/main/java/click/divichart/bean/dto/BarChartDto.java
@@ -0,0 +1,22 @@
+package click.divichart.bean.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BarChartDto implements Serializable {
+    private String[] recentYears;
+    private String targetYear;
+    private String chartData;
+
+    public BarChartDto(String[] recentYears, String targetYear, String chartData) {
+        this.recentYears = recentYears;
+        this.targetYear = targetYear;
+        this.chartData = chartData;
+    }
+}
