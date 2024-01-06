acc49089824519acacbce3475861937d24c1809e
Takuya-Fukumura <blackchoco1114@gmail.com>
2024-01-07 01:24:31 +0900

    メソッドのオーバーライドおよび実装には「@Override」を使用する必要がありますjava:S1161

--- src/main/java/click/divichart/configuration/MvcConfig.java
+++ src/main/java/click/divichart/configuration/MvcConfig.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         // URLパターンと遷移先viewファイル名
         registry.addViewController("/login").setViewName("login");
