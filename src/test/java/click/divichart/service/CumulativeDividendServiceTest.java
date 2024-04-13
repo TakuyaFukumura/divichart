8f4f381319aa2225411ab016e9327abbe356a210
Takuya-Fukumura <blackchoco1114@gmail.com>
2024-04-13 20:06:37 +0900

    tmp

---
+++ src/test/java/click/divichart/service/CumulativeDividendServiceTest.java
@@ -0,0 +1,28 @@
+package click.divichart.service;
+
+import click.divichart.repository.DividendHistoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CumulativeDividendServiceTest {
+
+    @Autowired
+    private DividendHistoryRepository repository;
+
+    private CumulativeDividendService cumulativeDividendService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getCumulativeDividend() {
+    }
+
+    @Test
+    void getLabels() {
+    }
+}
