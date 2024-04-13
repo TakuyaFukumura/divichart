0fc2fbc21eb63db9205595f45967c797fa472fd8
Takuya-Fukumura <blackchoco1114@gmail.com>
2024-04-13 20:14:08 +0900

    tmp

--- src/test/java/click/divichart/service/CumulativeDividendServiceTest.java
+++ src/test/java/click/divichart/service/CumulativeDividendServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CumulativeDividendServiceTest {
@@ -21,6 +23,18 @@ class CumulativeDividendServiceTest {
 
     @Test
     void getCumulativeDividend() {
+        // テストデータの準備
+        BigDecimal[] yearlyDividend = {BigDecimal.ONE, BigDecimal.TEN, BigDecimal.valueOf(5)};
+
+        // テスト実行
+        BigDecimal[] cumulativeDividend = cumulativeDividendService.getCumulativeDividend(yearlyDividend);
+
+        // 検証
+        assertNotNull(cumulativeDividend);
+        assertEquals(3, cumulativeDividend.length);
+        assertEquals(BigDecimal.ONE, cumulativeDividend[0]);
+        assertEquals(BigDecimal.valueOf(11), cumulativeDividend[1]);
+        assertEquals(BigDecimal.valueOf(16), cumulativeDividend[2]);
     }
 
     @Test
