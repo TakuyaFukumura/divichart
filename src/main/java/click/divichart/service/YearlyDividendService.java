f12f0ce6beefb372ccc554d20b402e3ec7425352
takuya-fukumura <blackchoco1114@gmail.com>
2025-03-07 00:13:12 +0900

    整形

--- src/main/java/click/divichart/service/YearlyDividendService.java
+++ src/main/java/click/divichart/service/YearlyDividendService.java
@@ -18,7 +18,7 @@ public class YearlyDividendService extends DividendService {
      * 指定された過去の各年に対する配当金の合計を取得し、それをリストとして返します。
      *
      * @param pastYears 過去の年を表す整数のリスト。このリストに含まれる各年について、対応する配当金の合計が計算されます。
-     * @param username 配当金情報を取得する対象のユーザー名。
+     * @param username  配当金情報を取得する対象のユーザー名。
      * @return 指定された各年における配当金の合計を格納したリスト。リストの要素は {@link BigDecimal} 型で、各年の配当金合計を表します。
      */
     public List<BigDecimal> getYearlyDividendData(List<Integer> pastYears, String username) {
