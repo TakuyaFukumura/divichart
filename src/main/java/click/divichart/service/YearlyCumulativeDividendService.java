484c40f0c195d6fab6df4de1715f74b800d1b521
takuya-fukumura <blackchoco1114@gmail.com>
2025-03-08 00:29:52 +0900

    delete

--- src/main/java/click/divichart/service/YearlyCumulativeDividendService.java
+++ src/main/java/click/divichart/service/YearlyCumulativeDividendService.java
@@ -17,19 +17,6 @@ public class YearlyCumulativeDividendService extends DividendService {
     }
 
     /**
-     * グラフ描画用に、指定年の1月～12月までの配当累計を計算する
-     *
-     * @param targetYear データ作成対象年
-     * @param username   ユーザ名
-     * @return グラフ描画用文字列
-     */
-    public String getChartData(String targetYear, String username) {
-        BigDecimal[] monthlyDividend = getMonthlyDividend(targetYear, username);
-        BigDecimal[] cumulativeDividend = getCumulativeDividend(monthlyDividend);
-        return createChartData(cumulativeDividend);
-    }
-
-    /**
      * 累計額の配列にして返す
      *
      * @param monthlyDividend 月別配当
